3ac2ff99b72ad14598c19d925b92094acd995b1c
Alexandre Manikowski <alexandre.karpinski@codeminer42.com>
2021-04-29 17:37:25 -0300

    dynamic URL for searchable pages

--- src/components/ui/SearchBar/SearchBar.js
+++ src/components/ui/SearchBar/SearchBar.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { isEmpty } from "ramda";
+import { useHistory, useLocation } from "react-router-dom";
 
 import Select from "react-select";
 
@@ -31,15 +33,44 @@ function SearchBar({
   open,
   close,
 }) {
+  const query = new URLSearchParams(useLocation().search);
+  const [selectValue, setSelectValue] = useState([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const queryItems = query.get("keys");
+
+    if (queryItems && !isEmpty(queryItems)) {
+      const parsedQueryItems = queryItems
+        .split(",")
+        .map((item) => ({ label: item }));
+      handleChange(parsedQueryItems);
+    }
+
+    /*eslint-disable-next-line */
+  }, []);
+
   function valueFromItem(item) {
     return searchTerm ? item[searchTerm] : item;
   }
 
-  const options = items.map((item) => {
-    return { label: valueFromItem(item), value: valueFromItem(item) };
-  });
+  const options = items.map((item) => ({
+    label: valueFromItem(item),
+    value: valueFromItem(item),
+  }));
+
+  const handleUrl = (selectedItems) => {
+    const currentPath = history.location.pathname;
+    const nextPathname = isEmpty(selectedItems)
+      ? currentPath
+      : `${currentPath}?keys=${selectedItems.sort().toString()}`;
+
+    history.replace(nextPathname);
+  };
 
   const handleChange = (selected) => {
+    setSelectValue(selected);
     const selectedItems = Array.isArray(selected)
       ? selected.map(({ label }) => label)
       : [selected.label];
@@ -48,6 +79,7 @@ function SearchBar({
       selectedItems.includes(valueFromItem(item))
     );
 
+    handleUrl(selectedItems);
     filterCallback(filteredItems);
   };
 
@@ -71,6 +103,7 @@ function SearchBar({
       menuIsOpen={isSearching}
       onFocus={open}
       onBlur={close}
+      value={selectValue}
     />
   );
 }
